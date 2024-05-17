bf6beb30e8dc5c7a3db997dce8b81fa743c6c803
TomDeclunder <tom.declunder@epitech.eu>
2024-05-17 23:09:12 +0200

    feat: adding wallpaper

--- graphic/graphic.c
+++ graphic/graphic.c
@@ -10,8 +10,13 @@
 int main()
 {
     sfRenderWindow* window;
-    sfVideoMode videoMode = {800, 600, 32};
+    sfVideoMode videoMode = {1920, 1080, 32};
     window = sfRenderWindow_create(videoMode, "ça crame ?", sfResize | sfClose, NULL);
+    sfTexture* texture = sfTexture_createFromFile("graphic/img/fond.jpg", NULL);
+    if (!texture)
+        return -1;
+    sfSprite* sprite = sfSprite_create();
+    sfSprite_setTexture(sprite, texture, sfTrue);
 
     while (sfRenderWindow_isOpen(window))
     {
@@ -22,8 +27,11 @@ int main()
                 sfRenderWindow_close(window);
         }
         sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_drawSprite(window, sprite, NULL);
         sfRenderWindow_display(window);
     }
+    sfSprite_destroy(sprite);
+    sfTexture_destroy(texture);
     sfRenderWindow_destroy(window);
     return 0;
 }
