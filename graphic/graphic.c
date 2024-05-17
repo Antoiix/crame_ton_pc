e6241eec13672c3b945aaee1b1cbd33d7b05b8c9
MartinDelangue <martin.delangue@epitech.eu>
2024-05-18 00:42:31 +0200

    [FEAT] main window

---
+++ graphic/graphic.c
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2024
+** crame_ton_pc
+** File description:
+** graphic.c
+*/
+
+#include "include/graphic.h"
+#include <SFML/Graphics.h>
+
+int main()
+{
+    sfRenderWindow* window;
+    sfVideoMode videoMode = {800, 600, 32};
+    window = sfRenderWindow_create(videoMode, "Affichage d'un carré", sfResize | sfClose, NULL);
+
+    while (sfRenderWindow_isOpen(window)) {
+        sfEvent event;
+        while (sfRenderWindow_pollEvent(window, &event)) {
+            if (event.type == sfEvtClosed)
+                sfRenderWindow_close(window);
+        }
+        sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_display(window);
+    }
+    sfRenderWindow_destroy(window);
+    return 0;
+}
---
+++ graphic/include/graphic.h
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-110 : Shell Boostrap
+** File description:
+** bootstrap.h
+*/
+
+#ifndef B_MUL_100_LIL_1_1_MYRADAR_MARTIN_DELANGUE_MY_H
+    #define B_MUL_100_LIL_1_1_MYRADAR_MARTIN_DELANGUE_MY_H
+
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <unistd.h>
+    #include <SFML/Graphics.h>
+    #include <SFML/System/Sleep.h>
+    #include <time.h>
+    #include <fcntl.h>
+    #include <math.h>
+    #include <SFML/Audio.h>
+
+#endif //B_MUL_100_LIL_1_1_MYRADAR_MARTIN_DELANGUE_MY_H
